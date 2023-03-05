6be906c1ea6eae69e1acef973f1ffedfd2731949
p4lee <palee@hotmail.fi>
2023-03-05 13:45:28 +0200

    df

--- src/components/budjettimatkat/Budjettimatkat.jsx
+++ src/components/budjettimatkat/Budjettimatkat.jsx
@@ -7,7 +7,7 @@ const Budjettimatkat = () => {
   return (
     <div class="budjettimatkat_container">
   <h2 className='budjettimatkat-h2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at elementum dui, at vulputate lacus.</h2>
-  <img className="budjetti_pic" src={IMG1}/>
+  <img className="budjetti_pic" src={IMG1} alt="Image" />
   <h3 className='fill-text'>
   Fusce eu tortor odio. Vivamus in vehicula quam, maximus consectetur est. In eu diam vitae arcu venenatis tristique quis a odio. Sed volutpat velit leo, eget feugiat lorem convallis at. Mauris tristique augue risus, id fringilla neque lacinia sed. Cras luctus eros lobortis, vestibulum nibh nec, tincidunt turpis. Aliquam viverra facilisis nulla nec imperdiet. Morbi sed ante tellus. Vivamus imperdiet iaculis dolor, in dapibus nisi ullamcorper finibus. Mauris mattis, sem nec consequat fermentum, lorem lectus lobortis nisi, ut congue ante ante sit amet ipsum. Nullam ornare fringilla tortor, in consectetur massa varius eu. Vestibulum quis sem enim.
 </h3>
